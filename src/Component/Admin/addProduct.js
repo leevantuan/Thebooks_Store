2c6834618db5e015777351a62a1a47ee311f947a
Ledangviet <73343108+Ledangviet@users.noreply.github.com>
2023-03-30 17:19:50 +0700

    modified:   src/Component/Admin/addProduct.js 	deleted:    src/Component/fetchData/getCategory.js

--- src/Component/Admin/addProduct.js
+++ src/Component/Admin/addProduct.js
@@ -1,30 +1,61 @@
 import styles from '../component.module.scss'
 import { FaAngleRight } from "react-icons/fa";
 import clsx from 'clsx';
-import { useCallback, useState } from 'react';
-import GetCategory from '../fetchData/getCategory';
+import { useCallback, useState, useEffect } from 'react';
+import loginCheck from '../Authen/loginCheck';
+
+
+
 
 export default function Product() {
+  const [cateList, setCateList] = useState([])
+  const [cateName, setCateName] = useState("")
+  const [cateDescription, setCateDescription] = useState("")
+  useEffect(() => {
+    fetch('https://thebookstore.azurewebsites.net/api/Category')
+      .then((res) => res.json())
+      .then((json) => {
+        setCateList(json);
+      });
+  }, []);
+  var token = localStorage.getItem('token');
 
-  var cateList = GetCategory();
-  console.log(cateList);
 
+  const id = 0;
+  const name = "Truyen trinh tham"
+  const description = "aaaaaaaaaaaaaaaaaaaaaa"
+  const createDate = "2023-03-30T10:05:00.556Z"
+
+  const AddCategory = () => {
+    console.log(cateName)
+    fetch('https://thebookstore.azurewebsites.net/api/Category', {
+      method: 'post',
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJhZG1pbkBlbWFpbC5jb20iLCJqdGkiOiI3YTYyNzkyMy1iNThjLTRkZGQtOWExYy1iZmY4ZTRjODFlMzYiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTY4MDE4MjQ4NSwiaXNzIjoiaHR0cHM6Ly90aGVib29rc3RvcmUuYXp1cmV3ZWJzaXRlcy5uZXQiLCJhdWQiOiJVc2VyIn0.E0F9bd8_MphTzhdtWIpqdFJ7AWPSmf06w243XdsNvZ8',
+        'Content-Type': 'application/json'
+      }
+      ,
+      body: JSON.stringify({
+        id,
+        name,
+        description,
+        createDate
+      }),
+
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+      });
+  }
   const [list, setList] = useState("");
   const [lists, setLists] = useState([]);
 
   const ramDomId = (min, max) => {
     return Math.floor(Math.random() * 100);
   }
-  const HandleClick = useCallback(() => {
-    const ramDomIdTl = ramDomId(0, 100);
-    setLists([...lists, { id: ramDomIdTl, nameTl: list }]);
-    setList("")
-  }, [list, lists])
 
 
-  const HandleInput = useCallback((e) => {
-    setList(e.target.value);
-  }, [])
 
   return (
     <>
@@ -40,20 +71,20 @@ export default function Product() {
         <div className={clsx(styles.type, styles.dsFlex)}>
           <label>Thể Loại</label>
           <input
-            onChange={HandleInput}
-            value={list}
-            placeholder='Nhập thể loại tại đây ... '
+            onChange={(e) => { setCateName(e.target.value) }}
+            placeholder='Category name ... '
+            required />
+          <input
+            onChange={(e) => { setCateDescription(e.target.value) }}
+            placeholder='Description ... '
             required />
           <button
-            disabled={!list}
-            onClick={HandleClick}
+            onClick={() => AddCategory()}
             className={styles.btnTL_admin} >Add</button>
-
-
         </div>
 
         <ol className={styles.tlAdmin} >
-          {lists.map((e) => <li key={e.id}>{e.nameTl}</li>)}
+          {cateList.map((e) => <li key={e.id}>{e.name}</li>)}
         </ol>
 
       </div>

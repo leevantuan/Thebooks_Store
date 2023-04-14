c0cf8884f17ed534d8f415a90cb78a811d8025e6
Ledangviet <73343108+Ledangviet@users.noreply.github.com>
2023-04-14 21:45:40 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,12 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
-import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </Router>
   </>
 );
